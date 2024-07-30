4f64aa9a0fab008a25ed702943f7ea0c34878a6d
Benjamin Cakmak <benjamin.cakmak@gmx.de>
2024-07-30 16:55:20 +0200

    Add main-function to call Shred

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"os"
+	"fmt"
+)
+
+func main() {
+	// Check for correct arguments.
+	if len(os.Args) != 2 {
+		fmt.Printf("Usage: %s <file_path>\n", os.Args[0])
+		os.Exit(1)
+	}
+	
+	path := os.Args[1]
+	err  := Shred(path)
+	if err != nil {
+		panic(err)
+	}
+}
